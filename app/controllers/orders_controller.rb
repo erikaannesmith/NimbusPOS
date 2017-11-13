9422f376be23a3c4b6def0b2d0ebdcbaf32a645b
Erika Smith <erikaannesmith@gmail.com>
2017-11-13 16:53:31 -0700

    Build out ability to create a new order for a particular service

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,26 @@
 class OrdersController < ApplicationController
 
+  def show
+    @order = Order.find(params[:id])
+  end
 
   def new
     @service = Service.find(params[:service_id])
     @order = @service.orders.new
+  end
+
+  def create
+    @service = Service.find(params[:service_id])
+    @order = @service.orders.create(order_params)
+    if @order.save
+      redirect_to service_order_path(@service, @order)
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:start_time, :end_time)
   end
 
 end
--- spec/features/user_can_view_all_services_spec.rb
+++ spec/features/user_can_view_all_services_spec.rb
@@ -13,4 +13,18 @@ describe "User visits services index" do
 
     expect(current_path).to eq(new_service_order_path(@service))
   end
+
+  it "they can create a new order" do
+    visit new_service_order_path(@service)
+
+    expect(page).to have_field("order[start_time]")
+    expect(page).to have_field("order[end_time]")
+
+    fill_in "order[start_time]", with: "2017-11-13 16:46:10 -0700"
+    fill_in "order[end_time]", with: "2017-11-13 16:50:10 -0700"
+
+    click_button "Create Order"
+
+    expect(current_path).to eq(service_order_path(@service, Order.last))
+  end
 end
